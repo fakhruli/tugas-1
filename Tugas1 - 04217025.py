f62e34edcd8ff9b4b251754d287ac459d97b9aec
fakhrul789 <fakhrulislam379@gmail.com>
2017-09-26 16:41:03 +0700

    tugas 1 nim 04217025

---
+++ Tugas1 - 04217025.py
@@ -0,0 +1,10 @@
+nama = "Mochammad Fakhrul islam"
+nim = "04217025"
+umur = 18
+
+print("nama = " + nama)
+print("nim = " + nim)
+print("umur = ")
+print(umur)
+print("Umur + 5 tahun =")
+print(umur + 5)
